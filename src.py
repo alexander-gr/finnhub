ea19b4c2a92995ee3f2a4a89d61dd912e6393b54
ChanitaT <91049368+ChanitaT@users.noreply.github.com>
2022-10-28 20:05:34 +0700

    Update src.py
    keep only one url

--- src.py
+++ src.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 from config import (
     URL_PRICE,
-    URL_PROFILE,
     SYMBOLS,
     TOKEN
 )
@@ -18,16 +17,14 @@ def main():
         }
 
     for symbol in SYMBOLS:
-        response_profile = requests.get(URL_PROFILE, params={'symbol': symbol, 'token': TOKEN})
         response_price = requests.get(URL_PRICE, params={'symbol': symbol, 'token': TOKEN})
-        print("+ " + str(response_profile.json()['ticker']))
+        # print("+ " + symbol)
 
-        results['stock_symbol'].append(response_profile.json()['ticker'])
+        results['stock_symbol'].append(symbol)
         results['percentage_change'].append(response_price.json()['dp'])
         results['current_price'].append(response_price.json()['c'])
         results['last_close_price'].append(response_price.json()['o'])
         df = pd.DataFrame(results, columns=['stock_symbol', 'percentage_change', 'current_price', 'last_close_price'])
         df.to_csv('most_volatile_stock.csv', index=False)
 
-    df2 = pd.read_csv('most_volatile_stock.csv')
-    print(df2)
+    print(df)
